880acd3f59979b5efdb1713f7b0f8b51740aafe1
ocjarman <ocjarman@gmail.com>
2022-10-17 12:19:16 -0400

    added tasks working!

--- src/App.js
+++ src/App.js
@@ -7,26 +7,44 @@ const dummyItems = ["drink a lot of coffee", "walk Karat", "feed Dean", "code"];
 
 //this is the parent/main component where states live
 function App() {
-  const [toDoItems] = useState(dummyItems);
+  const [toDoItems, setToDoItems] = useState(dummyItems);
   const [filter, setFilter] = useState("all"); // //'all' is the default value for selected filter
-
+  const [newTask, setNewTask] = useState("");
   // state is handled in the component and can update it inside component.
   // when you change the state inside your app, itll re-render that section of the app
   // props are handled outside of component, must be updated outside component
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    //make an array composed of everything in original list, plus new added tasks
+    const newToDoList = [...toDoItems, newTask];
+    //set original list to the new list!
+    setToDoItems(newToDoList);
+  };
 
   return (
     <div className="App">
       <h1>To Dos:</h1>
       <div className="container">
         <div id="list">
-          {toDoItems.map((item) => {
-            return <SingleToDo key={item} toDoName={item} filter={filter} />;
-          })}
+          <ul>
+            <li>
+              {toDoItems.map((item) => {
+                return (
+                  <SingleToDo key={item} toDoName={item} filter={filter} />
+                );
+              })}
+            </li>
+          </ul>
         </div>
       </div>
       {/*---------------form just for show --------*/}
-      <input type="text" placeholder="add new todo" />
-      <input id="submitBtn" type="submit" value="add" />
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
       <br></br>
       <Filter filter={filter} setFilter={setFilter} />
     </div>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,14 +5,15 @@ export const Filter = ({filter, setFilter}) => {
     
     //to make obvious which is selected
     const currentFilter = {boxShadow: '2px 2px'}
+    const changeValue = (event) => setFilter(event.target.value)
     
     //setFilter function in line 13 of App.js can be used here to change the 'filter' state
     //when a button is pressed, its value is set as the 'filter' through the setFilter func inherited by the main componet in app
     return (
     <div>
-    <button style={filter==='all' ? currentFilter : null } value="all" onClick={(event) => setFilter(event.target.value)}>All</button>
-    <button style={filter==='active' ? currentFilter : null }value="active" onClick={(event) => setFilter(event.target.value)}>Active</button>
-    <button style={filter==='completed' ? currentFilter : null }value="completed" onClick={(event) => setFilter(event.target.value)}>Completed</button>
+    <button style={filter==='all' ? currentFilter : null } value="all" onClick={changeValue}>All</button>
+    <button style={filter==='active' ? currentFilter : null }value="active" onClick={changeValue}>Active</button>
+    <button style={filter==='completed' ? currentFilter : null }value="completed" onClick={changeValue}>Completed</button>
     </div> 
     )
 }
